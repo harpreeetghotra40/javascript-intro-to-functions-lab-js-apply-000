006e4bb2dc4acb5784cc89c273e5c1b236223258
HARPREET GHOTRA <harpreetghotra40@icloud.com>
2019-02-06 22:00:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,7 +15,10 @@ function logWhisper(string){
 }
 
 function sayHiToGrandma(string){
-  if(string === string.toLowerCase()){
+  if(string === "I love you, Grandma."){
+    return
+  }
+  else if(string === string.toLowerCase()){
     return "I can't hear you!"
   }
   else if(string === string.toUpperCase()){
